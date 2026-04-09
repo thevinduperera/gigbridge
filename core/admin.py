7258854b9d6d91592d9299d82054c272daae3c9d
Akila shamnaka <akilashamnaka12@gmail.com>
2026-04-09 15:07:46 +0530

    feat: add Notification model to core app

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['recipient', 'notif_type', 'message', 'is_read', 'created_at']
+    list_filter  = ['notif_type', 'is_read']
+    search_fields = ['recipient__username', 'message']
--- gigbridge/settings.py
+++ gigbridge/settings.py
@@ -31,17 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    # Our apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
     'accounts',
     'tasks',
     'proposals',
-    'core',
+    'core',          
 ]
 
 MIDDLEWARE = [
