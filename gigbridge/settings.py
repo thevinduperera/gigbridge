47c7ead1502cf3f28b89e575814f0cfe8779ed34
Akila shamnaka <akilashamnaka12@gmail.com>
2026-04-13 01:23:10 +0530

    ≈y feat: add notification context processor for bell count

--- gigbridge/settings.py
+++ gigbridge/settings.py
@@ -66,11 +66,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-<<<<<<< HEAD
-                'proposals.context_processors.proposal_count',
-=======
-                "core.context_processors.notifications_count",
->>>>>>> 9db20fa (feat: add notification context processor for bell count)
             ],
         },
     },
