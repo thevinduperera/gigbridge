ed4ab57a56edde2549b89cc3728f41c9ca4e3350
LinalManamperi <manamperilinal@gmail.com>
2026-04-12 21:00:41 +0530

    feat: add proposal count context processor

--- gigbridge/settings.py
+++ gigbridge/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'proposals.context_processors.proposal_count',
             ],
         },
     },
