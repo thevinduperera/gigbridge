5a9a6e5533113a3d7604fa71e4b2549d825bfe67
dimeth <dimethvindiv16@gmail.com>
2026-04-21 10:46:29 +0530

    profiles: add client preview + consistent styling

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,8 +23,31 @@ class FreelancerProfileForm(forms.ModelForm):
             'profile_picture',
         ]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name == 'bio':
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'rows': 4,
+                    'placeholder': f'Enter {field.label.lower()}'
+                })
+            else:
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'placeholder': f'Enter {field.label.lower()}'
+                })
+
 
 class ClientProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'company_name', 'website', 'location', 'profile_picture']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': f'Enter {field.label.lower()}'
+            })
